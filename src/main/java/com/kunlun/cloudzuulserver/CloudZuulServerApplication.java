987d7c00386ccbadb894b80a698cbc116727c83e
Jarven <769558579@qq.com>
2018-01-02 09:37:51 +0800

    分布式服务网关

---
+++ src/main/java/com/kunlun/cloudzuulserver/CloudZuulServerApplication.java
@@ -0,0 +1,12 @@
+package com.kunlun.cloudzuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudZuulServerApplication.class, args);
+	}
+}
